4d25b39dfa15c6d01c77d1ecd9495d5aae0946e6
hjmr <2620990+hjmr@users.noreply.github.com>
2024-10-26 12:16:37 +0900

    add simple codes.

--- tktest.py
+++ tktest.py
@@ -1,21 +1,36 @@
 import tkinter as tk
 
+
+def btn_click(hello):
+    name_str = name_txt.get()
+    msg_txt.config(state=tk.NORMAL)
+    msg_txt.insert(tk.END, f"{hello}, {name_str}.\n")
+    msg_txt.config(state=tk.DISABLED)
+
+
 root = tk.Tk()
 root.title("Hello, Tkinter!")
 root.geometry("300x200")
 
-frame = tk.Frame(root)
-frame.pack(side=tk.TOP)
+btn_frame = tk.Frame(root)
+btn_frame.pack(side=tk.TOP)
 
-btn1 = tk.Button(frame, text="Button 1")
-btn2 = tk.Button(frame, text="Button 2")
+btn1 = tk.Button(btn_frame, text="Hello", command=lambda: btn_click("Hello"))
+btn2 = tk.Button(btn_frame, text="Bonjour", command=lambda: btn_click("Bonjour"))
 
 btn1.pack(side=tk.LEFT)
 btn2.pack(side=tk.LEFT)
 
-txt = tk.Text(root, height=5, width=25)
-txt.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+name_frame = tk.Frame(root)
+name_frame.pack(side=tk.TOP)
 
-txt.bind("<KeyPress>", lambda e: print(e))
+name_label = tk.Label(name_frame, text="Name:")
+name_txt = tk.Entry(name_frame)
+
+name_label.pack(side=tk.LEFT)
+name_txt.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+msg_txt = tk.Text(root, wrap=tk.WORD, state=tk.DISABLED)
+msg_txt.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 root.mainloop()
